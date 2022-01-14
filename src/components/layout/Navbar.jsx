7e91adf93023bddcea420bdb4dc13a6d40e275e3
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-14 21:36:50 +0200

    added pages components

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FaGithub} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import '../../index.css';
 
 
 function Navbar({title}) {
@@ -9,7 +10,7 @@ function Navbar({title}) {
             <div className='container mx-auto'>
                 <div className='flex-none px-2 mx-2'>
                     <FaGithub className='inline pr-2 text-4xl'/>
-                    <Link to='/' className='text-lg font-bold align-middle'>
+                    <Link to='/' id='text-logo' className='text-lg font-bold align-middle'>
                         {title}
                     </Link>
                 </div>
