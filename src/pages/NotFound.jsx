5460718927fbece0087032feca853eb883711d3e
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-15 23:09:39 +0200

    basic UserResults added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ function NotFound() {
             <div className="text-center hero-content">
                 <div className="max-w-lg">
                     <h1 className="text-8xl font-bold mb-8">
-                        Oops!
+                        Oops :(
                     </h1>
                     <p className="text-5xl mb-8">404 - Page not found!</p>
                     <Link to='/' className='btn btn-primary btn-lg'>
