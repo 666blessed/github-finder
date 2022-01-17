ab0849c7e11afad9217e8978e86d79234cfeb043
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-17 15:46:10 +0200

    search functionality updated

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -5,14 +5,14 @@ import GithubContext from '../../context/github/GithubContext';
 function UserSearch() {
     const [text, setText] = useState('');
     const handleChange = e => setText(e.target.value);
-    const {users} = useContext(GithubContext);
+    const {users, searchUsers} = useContext(GithubContext);
 
     const handleSubmit = e => {
         e.preventDefault();
         if (text === '') {
             alert('Please enter something')
         } else {
-            //todo - search users
+            searchUsers(text);
             setText('');
         }
     }
