195a814997b996afea809f646f10137f3f2a600a
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-17 18:37:19 +0200

    Alert context setup

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from "react";
+import alertReducer from './AlertReducer';
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({children}) => {
+    const initialState = null;
+
+    const [state, dispatch] = useReducer(alertReducer, initialState);
+
+    return <AlertContext.Provider>
+        {children}
+    </AlertContext.Provider>
+}
+
+export default AlertContext;
