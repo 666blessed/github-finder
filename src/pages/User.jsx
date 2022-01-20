02307f7a25c309fa5eb8817f15e81c2409aca3b5
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-20 17:32:07 +0200

    User page UI updates

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,7 +3,7 @@ import {userEffect, useContext} from 'react';
 import { useEffect } from 'react/cjs/react.development';
 import GithubContext from '../context/github/GithubContext';
 import {useParams} from 'react-router-dom';
-import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa';
+import {FaCodepen, FaStore, FaUserFriends, FaUsers, FaCode} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
 
@@ -76,9 +76,97 @@ function User() {
               </a>
             </div>
           </div>
+
+          <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+            {location && (
+              <div className="stat">
+                <div className="stat-title text-md">
+                  Location
+                </div>
+                <div className="text-lg stat-value">
+                  {location} 
+                </div>
+              </div>
+            )}
+            {blog && (
+              <div className="stat">
+                <div className="stat-title text-md">
+                  Website
+                </div>
+                <div className="text-lg stat-value">
+                  <a href={`${blog}`} target="_blank" rel="noreferrer">
+                    {blog}
+                  </a> 
+                </div>
+              </div>
+            )}
+            {twitter_username && (
+              <div className="stat">
+                <div className="stat-title text-md">
+                  Twitter
+                </div>
+                <div className="text-lg stat-value">
+                  <a href={`https://twitter.com/${twitter_username}`} target="_blank" rel="noreferrer">
+                    {twitter_username}
+                  </a> 
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
+      <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Followers
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {followers}
+          </div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUserFriends className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Following
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {following}
+          </div>
+        </div>
+
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaCode className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Public Repos
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {following}
+          </div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaStore className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Public Gists
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {public_gists}
+          </div>
+        </div>
+
+      </div>
     </div>
     
   </>;
